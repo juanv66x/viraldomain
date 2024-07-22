450ae43918b8153eaa9110e5b7b5082408240f69
Juan Acuna <acua6307@egmail.mx>
2024-07-21 20:56:55 -0600

    Addition of sero data set

---
+++ data-raw/sero-data.R
@@ -0,0 +1,10 @@
+# load required packages ----
+if (!require("pacman")) install.packages("pacman") 
+pacman::p_load(dplyr)
+
+# clean data ----
+sero = sero |>
+  transmute(cd_2022 = jitter(cd_2022), vl_2022 = jitter(vl_2022))
+
+# write data in correct format to data folder ----
+usethis::use_data(sero, overwrite = TRUE)
